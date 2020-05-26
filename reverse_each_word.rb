f2a680f2ecf5e8cc86543786b6a18c59ccb3a039
Shiro Han <shan99business@gmail.com>
2020-05-26 21:11:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
+require 'pry'
+
 def reverse_each_word(string)
   myArray = string.split(" ")
-  output = []
   myArray.each do |word|
     output << word.reverse
   end
-  output
 end
+
+binding.pry
