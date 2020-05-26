7f7a3d5362c2e794be77f18ec511f4eb2acf3fed
Shiro Han <shan99business@gmail.com>
2020-05-26 21:18:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,17 @@
 require 'pry'
 
-def reverse_each_word(string)
-  myArray = string.split(" ")
-  output = ""
-  myArray.each do |word|
-    output << word.reverse + " "
+#def reverse_each_word(string)
+#  myArray = string.split(" ")
+#  output = ""
+#  myArray.each do |word|
+#    output << word.reverse + " "
+#  end
+#  output.chop
+#end
+
+def reverse_each_wrod(string)
+  myArray = string.split("")
+  myArray.collect do |word|
+    word.reverse
   end
-  output.chop
 end
